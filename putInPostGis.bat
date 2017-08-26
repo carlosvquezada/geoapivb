d763912f32bac9125b5119b91198e20dfbc79270
Carlos V. Quezada <carlosvquezada@yahoo.com>
2017-08-25 20:27:42 -0400

    Add files via upload

---
+++ putInPostGis.bat
@@ -0,0 +1,9 @@
+c:
+cd\Program Files\GDAL
+GDALShell.bat
+cd\project\Shape Retrieval 3.0
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_lines.geojson" -nln manhattan_building_lines 
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_multipolygons.geojson" -nln manhattan_building_poly
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_part_lines.geojson" -nln manhattan_building_part_lines 
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_part_multipolygons.geojson" -nln manhattan_building_part_poly
+
