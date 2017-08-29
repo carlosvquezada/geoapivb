eb87e4f79b796013c821ddf9fa665ac8c9f81de7
Carlos V. Quezada <carlosvquezada@yahoo.com>
2017-08-29 14:40:54 -0400

    Add files via upload

--- putInPostGis.bat
+++ putInPostGis.bat
@@ -1,9 +1,25 @@
 c:
-cd\Program Files\GDAL
-CALL GDALShell.bat
-cd\project\Shape Retrieval 3.0
-ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_lines.geojson" -nln manhattan_building_lines 
-ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_multipolygons.geojson" -nln manhattan_building_poly
-ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_part_lines.geojson" -nln manhattan_building_part_lines 
-ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=gis user=postgres password=postgres" "building_part_multipolygons.geojson" -nln manhattan_building_part_poly
+@echo off
+@echo Setting environment for using the GDAL Utilities.
+set SDK_ROOT=%~dp0
+set SDK_ROOT=%SDK_ROOT:\\=\%
+SET "PATH=%SDK_ROOT%;%PATH%"
+SET "GDAL_DATA=%SDK_ROOT%gdal-data"
+SET "GDAL_DRIVER_PATH=%SDK_ROOT%gdalplugins"
+SET "PROJ_LIB=%SDK_ROOT%projlib"
+SET "PYTHONPATH=%SDK_ROOT%python"
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Load data into PostGIS (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
+@echo on
+cd\project\Shape Retrieval 3.0
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=testdoc user=postgres password=postgres" "building_lines.geojson" -nln nyc_building_lines 
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=testdoc user=postgres password=postgres" "building_multipolygons.geojson" -nln nyc_building_poly
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=testdoc user=postgres password=postgres" "part_lines.geojson" -nln nyc_part_lines 
+ogr2ogr -f "PostgreSQL" "PG:host=localhost dbname=testdoc user=postgres password=postgres" "part_multipolygons.geojson" -nln nyc_part_poly
+
+:END
+PAUSE
+endlocal
